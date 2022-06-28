07f443db35fcf150d184232be0bce6993f377dc7
gauravksh <73842400+gauravksh@users.noreply.github.com>
2022-06-28 23:24:23 +0530

    Time: 89 ms (38.37%), Space: 23 MB (57.13%) - LeetHub

--- 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.cpp
+++ 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.cpp
@@ -1,22 +1,16 @@
 class Solution {
-    bool isPalindrome(string &s) {
-        int l = 0, r = s.length()-1;
-        while (l < r) {
-            if (s[l] != s[r]) {
-                return false;
-            }
-            l++;
-            r--;
-        }
-        
-        return true;
-    }
 public:
     string firstPalindrome(vector<string>& words) {
-        for(string word : words) {
-            if (isPalindrome(word)) {
-                return word;
+        for(string i : words) {
+            int l = 0, r = i.length() - 1;
+            while (l < r) {
+                if (i[l]==i[r]) {
+                    l++;
+                    r--;
+                }
+                else break;
             }
+            if (l >= r) return i;
         }
         return "";
     }
