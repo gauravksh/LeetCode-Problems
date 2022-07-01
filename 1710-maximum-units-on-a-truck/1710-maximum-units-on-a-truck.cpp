795a7de2fbd0f207151f2d89d85226a52f3baf99
gauravksh <73842400+gauravksh@users.noreply.github.com>
2022-07-01 13:56:32 +0530

    Time: 55 ms (73.26%), Space: 16 MB (53.04%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
@@ -1,19 +1,27 @@
 class Solution {
-    static bool comparator(vector<int>& a,vector<int>& b) {
+    static bool comparator(vector<int> &a,vector<int> &b) {
         return a[1] > b[1];
     }
     
 public:
     int maximumUnits(vector<vector<int>>& boxTypes, int truckSize) {
         sort (boxTypes.begin(),boxTypes.end(),comparator);
-        int maxUnits = 0, totBoxes = 0, i = 0;
+        int maxUnits = 0;
         
-        while(totBoxes <= truckSize && i < boxTypes.size()) {
-            totBoxes += boxTypes[i][0];
-            maxUnits += boxTypes[i][0]*boxTypes[i++][1];
+        maxUnits = min(truckSize, boxTypes[0][0]) * boxTypes[0][1];
+        truckSize -= boxTypes[0][0];
+        
+        for (int i = 1; i < boxTypes.size(); i++) {
+            int boxes = boxTypes[i][0], units = boxTypes[i][1];
+            if (truckSize > 0) {
+                maxUnits += min(truckSize, boxes) * units;
+                truckSize -= min(truckSize, boxes);
+            }
+            else {
+                break;
+            }
         }
-        if (totBoxes > truckSize) 
-            return maxUnits - (totBoxes - truckSize) * boxTypes[--i][1];
-        else return maxUnits;
+        
+        return maxUnits;
     }
 };
