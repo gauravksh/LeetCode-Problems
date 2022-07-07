8ad4d887f9737148a77e5ee47ccef6b2379c21b8
gauravksh <73842400+gauravksh@users.noreply.github.com>
2022-07-07 13:48:12 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (48.95%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,30 +1,31 @@
 class Solution {
-public:
-    // int dp[102][102];
-    bool helper(string& s1, string& s2, string &s3, int l1, int l2, int l3, vector<vector<int>>&dp) {
-        if(l1<0 && l2<0 && l3<0) {
+    bool helper(string& s1, string& s2, string &s3, int l1, int l2, int l3, vector<vector<int>> &dp) {
+        
+        if(l1 < 0 && l2 < 0 && l3 < 0) {
             return true;
         }
-        if(l1>=0 && l2>=0 && dp[l1][l2]!=-1) {
+        if(l1 >= 0 && l2 >= 0 && dp[l1][l2] != -1)
             return dp[l1][l2];
-        }
-        if(l1>=0 && s1[l1]==s3[l3] && l2>=0 && s2[l2]==s3[l3] ) {
+        
+        if(l1 >= 0 && s1[l1] == s3[l3] && l2 >= 0 && s2[l2] == s3[l3] )
             return dp[l1][l2] = (helper(s1, s2, s3, l1-1, l2, l3-1, dp) || helper(s1, s2, s3, l1, l2-1, l3-1, dp));
-        }
-        else if(l1>=0 && s1[l1]==s3[l3])
+        else if(l1 >= 0 && s1[l1] == s3[l3])
             return helper(s1, s2, s3, l1-1, l2, l3-1, dp);
         
-        else if(l2>=0 &&s2[l2]==s3[l3])
+        else if(l2 >= 0 && s2[l2] == s3[l3])
             return helper(s1, s2, s3, l1, l2-1, l3-1, dp);
         
-        else return false;
+        return false;
     }
     
+public:
+    
     bool isInterleave(string s1, string s2, string s3) {
-        if(s3.size() != s1.size() + s2.size())
-			return false;
         vector<vector<int>>dp(s1.size()+1, (vector<int>(s2.size()+1, -1)));
-        // memset(dp, -1, sizeof(dp));
-        return helper(s1, s2, s3,s1.size()-1, s2.size()-1, s3.size()-1, dp);
+        if(s3.length() != s1.length() + s2.length())
+			return false;
+        if(!s3.length() && !s1.length() && !s2.length())
+            return true;
+        return helper(s1, s2, s3, s1.length()-1, s2.length()-1, s3.length()-1, dp);
     }
 };
