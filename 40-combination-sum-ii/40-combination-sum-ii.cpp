275e00f6f27bbe14f989739de7a4ff49bc603b72
gauravksh <73842400+gauravksh@users.noreply.github.com>
2022-07-08 11:38:10 +0530

    Time: 29 ms (15.14%), Space: 10.8 MB (46.65%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.cpp
+++ 40-combination-sum-ii/40-combination-sum-ii.cpp
@@ -1,23 +1,31 @@
 class Solution {
-    void solve(vector<vector<int>> &ans, vector<int> &cur, vector<int> &arr, int target, int idx) {
-        if(target == 0) {
-            ans.push_back(cur);
+    void solve(set<vector<int>> &ans, vector<int> &cur, vector<int> &arr, int target, int idx) {
+        if(target  < 0) return;
+        if(idx >= arr.size()) return;
+        if(arr[idx] == target) {
+            cur.push_back(arr[idx]);
+            ans.insert(cur);
+            cur.pop_back();
             return;
         }
-        for(int i = idx; i < arr.size(); i++) {
-            if(i > idx && arr[i] == arr[i - 1]) continue;
-            if(arr[i] > target) break;
-            cur.push_back(arr[i]);
-            solve(ans, cur, arr, target - arr[i], i + 1);
-            cur.pop_back();
-        }
+        cur.push_back(arr[idx]);
+        solve(ans, cur, arr, target - arr[idx], idx + 1);
+        cur.pop_back();
+        int i = idx + 1;
+        while(i < arr.size() && arr[i] == arr[idx]) i++;
+        solve(ans, cur, arr, target, i);
     }
 public:
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
         sort(candidates.begin(), candidates.end());
-        vector<vector<int>> ans;
+        set<vector<int>> ans;
         vector<int> cur;
         solve(ans, cur, candidates, target, 0);
-        return ans;
+        vector<vector<int>> res;
+        for(auto x : ans) {
+            res.push_back(x);
+        }
+        
+        return res;
     }
 };
