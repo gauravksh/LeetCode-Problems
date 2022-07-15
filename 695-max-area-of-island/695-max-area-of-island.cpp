62b064526028bb9ad86384fa734c0d650f4746b4
gauravksh <73842400+gauravksh@users.noreply.github.com>
2022-07-15 12:50:46 +0530

    Time: 27 ms (65.32%), Space: 24.3 MB (22.76%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -1,44 +1,49 @@
 class Solution {
-    int vis[55][55];
-    int solve(int ans, int cur, int row, int col, vector<vector<int>>& grid, int m, int n) {
-        if(row+1 < n && grid[row+1][col] == 1 && vis[row+1][col] == -1) {
-            vis[row+1][col] = 1;
-            cur += solve(ans, 1, row+1, col, grid, m, n);
-        }
-        if(row-1 >= 0 && grid[row-1][col] == 1 && vis[row-1][col] == -1) {
-            vis[row-1][col] = 1;
-            cur += solve(ans, 1, row-1, col, grid, m, n);
-        }   
-        if(col+1 < m && grid[row][col+1] == 1 && vis[row][col+1] == -1) {
-            vis[row][col+1] = 1;
-            cur += solve(ans, 1, row, col+1, grid, m, n);
-        }   
-        if(col-1 >= 0 && grid[row][col-1] == 1 && vis[row][col-1] == -1) {
-            vis[row][col-1] = 1;
-            cur += solve(ans, 1, row, col-1, grid, m, n);
-        }
-        ans = max(cur,ans);
-        // cout << row << col << ans << endl;
-        return ans;
-
-    }
+    vector<vector<bool>> vis;
+    vector<vector<int>> grid;
+    int M, N;
+    
+    int dirs[4][2] = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+    
 public:
+    bool isValid(int i, int j) {
+        if (i < 0 || j < 0 || i >= M || j >= N ||
+            vis[i][j] || grid[i][j] == 0) {
+            return false;
+        }
+        return true;
+    }
+    
+    int dfs(int i, int j) {
+        vis[i][j] = true;
+        
+        int cur_area = 1;
+        for (auto dir : dirs) {
+            int x = i + dir[0];
+            int y = j + dir[1];
+            
+            if (isValid(x, y)) {
+                cur_area += dfs(x, y);
+            }
+        }
+        
+        return cur_area;
+    }
+    
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-        memset(vis, -1, sizeof(vis));
-        int m = grid[0].size();
-        int n = grid.size();
-        int cur = 0;
-        int ans = 0;
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
-                if(vis[i][j] == -1 && grid[i][j] == 1) {
-                    cur++;
-                    vis[i][j] = 1;
-                    ans = max(ans,solve(0, 1, i, j, grid, m, n));
-                    cur--;
+        this->grid = grid;
+        M = grid.size(), N = grid[0].size();
+        vis = vector<vector<bool>> (M, vector<bool>(N, false));
+        
+        int max_area = 0;
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < N; j++) {
+                if (grid[i][j] && !vis[i][j]) {
+                    max_area = max(max_area, dfs(i, j));
                 }
             }
         }
-        return ans;
+        
+        return max_area;
     }
 };
