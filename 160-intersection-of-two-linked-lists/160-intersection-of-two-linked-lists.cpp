117c9899fd2fa4f995edc1f4e2e084a2e2a3dda5
gauravksh <73842400+gauravksh@users.noreply.github.com>
2022-07-20 15:04:07 +0530

    Time: 122 ms (11.20%), Space: 21.2 MB (5.49%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -9,32 +9,15 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        unordered_map<int, ListNode*> mpA;
-        unordered_map<int, ListNode*> mpB;
-
-        while(headA && headB) {
-            if(mpB.find(headA->val) != mpB.end() && mpB[headA->val] == headA)
-                return headA;
-            mpA[headA->val] = headA;
-            headA = headA->next;
-            if(mpA.find(headB->val) != mpA.end() && mpA[headB->val] == headB)
-                return headB;
-            mpB[headB->val] = headB;
-            headB = headB->next;
-        }
+        unordered_map<ListNode*,int> mp;
         while(headA) {
-            if(mpB.find(headA->val) != mpB.end() && mpB[headA->val] == headA)
-                return headA;
-            mpA[headA->val] = headA;
+            mp[headA]++;
             headA = headA->next;
         }
         while(headB) {
-            if(mpA.find(headB->val) != mpA.end() && mpA[headB->val] == headB)
-                return headB;
-            mpB[headB->val] = headB;
+            if(mp[headB] > 0) return headB;
             headB = headB->next;
         }
-        
-        return nullptr;
+        return NULL;
     }
 };
