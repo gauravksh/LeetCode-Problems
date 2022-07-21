9fd5c904cbc3e971e36996b76d3965ca4a1e2c71
gauravksh <73842400+gauravksh@users.noreply.github.com>
2022-07-21 23:33:29 +0530

    Time: 1201 ms (39.34%), Space: 253.2 MB (65.22%) - LeetHub

--- 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.cpp
+++ 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.cpp
@@ -1,22 +1,27 @@
 class Solution {
 public:
     ListNode* mergeNodes(ListNode* head) {
-        ListNode* res = new ListNode(0);
-        ListNode* ans = res;
+        ListNode* ans = head;
         
         ListNode* ptr = head->next;
         int cur_sum = 0;
+        
         while (ptr) {
             while (ptr->val != 0) {
                 cur_sum += ptr->val;
                 ptr = ptr->next;
             }
-            res->next = new ListNode(cur_sum);
-            res = res->next;
+            ans->val = cur_sum;
             cur_sum = 0;
+            if (ptr->next == nullptr) {
+                ans->next = nullptr;
+                break;
+            }
+            ans->next = ptr;
+            ans = ans->next;
             ptr = ptr->next;
         }
         
-        return ans->next;
+        return head;
     }
 };
