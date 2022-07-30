d0113a8d09a0876a6c812258ad3fd514a76912e0
gauravksh <73842400+gauravksh@users.noreply.github.com>
2022-07-30 19:13:46 +0530

    Time: 5 ms (50.00%), Space: 7.4 MB (48.67%) - LeetHub

---
+++ 1111-maximum-nesting-depth-of-two-valid-parentheses-strings/1111-maximum-nesting-depth-of-two-valid-parentheses-strings.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<int> maxDepthAfterSplit(string seq) {
+        int open = 0, mx = 0;
+        vector<int> ans(seq.size());
+        for(char ch : seq) {
+            if(ch == '(') open++;
+            else open--;
+            mx = max(open,mx);
+        }
+        open = 0;
+        for(int i = 0; i < seq.size(); i++) {
+            if(seq[i] == '(' && open + 1 <= mx/2) {
+                ans[i] = 0;
+                open++;
+            }
+            else if(seq[i] == ')' && open <= mx/2) {
+                ans[i] = 0;
+                open--;
+            }
+            else {
+                if(seq[i] == '(') {
+                    open++;
+                    ans[i] = 1;
+                }
+                else {
+                    open--;
+                    ans[i] = 1;
+                }
+            }
+        }
+        return ans;
+    }
+};
