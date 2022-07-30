af458d72fa2fd8be2fbac1f85a1c25f0e61d667d
gauravksh <73842400+gauravksh@users.noreply.github.com>
2022-07-30 13:45:10 +0530

    Time: 289 ms (59.55%), Space: 57.4 MB (90.79%) - LeetHub

--- 916-word-subsets/916-word-subsets.cpp
+++ 916-word-subsets/916-word-subsets.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
-        vector<int> mp(26);
+        vector<int> mp(26), cur(26);
         vector<string> res;
         for(auto x : words2) {
-            vector<char> cur(26);
+            fill(begin(cur), end(cur), 0);
             for(char ch : x) {
                 cur[ch - 'a']++;
                 if(cur[ch - 'a'] > mp[ch - 'a']) mp[ch - 'a'] = cur[ch - 'a'];
@@ -12,11 +12,11 @@ public:
         }
         
         for(auto x : words1) {
-            vector<int> tmp(26);
-            for(char ch : x) tmp[ch - 'a']++;
+            fill(begin(cur), end(cur), 0);
+            for(char ch : x) cur[ch - 'a']++;
             int i;
             for(i = 0; i < 26; i++) {
-                if(mp[i] > tmp[i]) break;
+                if(mp[i] > cur[i]) break;
             }
             if(i == 26) res.push_back(x);
         }
