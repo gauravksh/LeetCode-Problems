dcde2763c1bd30fff7b5aef0d9ef9eb2990bfe10
gauravksh <73842400+gauravksh@users.noreply.github.com>
2022-07-30 15:39:15 +0530

    Time: 8 ms (69.45%), Space: 10.7 MB (16.55%) - LeetHub

--- 71-simplify-path/71-simplify-path.cpp
+++ 71-simplify-path/71-simplify-path.cpp
@@ -3,24 +3,13 @@ public:
     string simplifyPath(string path) {
         stack<string> dir; 
         string ans = "";
-        int i = 0;
-        while(i < path.size()) {
-            if(path[i] == '/') i++;
-            else if(path[i] == '.') {
-                int cur = i;
-                int len = 0;
-                while(path[i] != '/' && i < path.size()) {
-                    if(path[i] == '.') len++;
-                    else len = 0;
-                    i++;
-                }
-                if(len > 2 || len == 0) dir.push(path.substr(cur,i - cur));
-                else if(len == 2 && !dir.empty()) dir.pop();
-            }
-            else {
-                int cur = i;
-                while(path[i] != '/' && i < path.size()) i++;
-                dir.push(path.substr(cur,i - cur));
+        string x;
+        stringstream ss(path);
+        while(getline(ss,x, '/')) {
+            if(x == "" || x == ".") continue;
+            else if(x == ".." && !dir.empty()) dir.pop();
+            else if(x != "..") {
+                dir.push(x);
             }
         }
         while(!dir.empty()) {
