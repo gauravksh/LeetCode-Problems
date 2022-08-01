1f48bf17b726d687b6a5181cfa55ba09ee7e9c3a
gauravksh <73842400+gauravksh@users.noreply.github.com>
2022-08-01 19:05:20 +0530

    Time: 5 ms (13.66%), Space: 5.9 MB (66.64%) - LeetHub

--- 556-next-greater-element-iii/556-next-greater-element-iii.cpp
+++ 556-next-greater-element-iii/556-next-greater-element-iii.cpp
@@ -2,8 +2,23 @@ class Solution {
 public:
     int nextGreaterElement(int n) {
         string dig = to_string(n);
-        next_permutation(begin(dig), end(dig));
+        int idx = -1;
+        for(int i = dig.size() - 2; i >= 0; i--) {
+            if(dig[i] < dig[i+1]) {
+                idx = i;
+                break;
+            }
+        }
+        if(idx == -1) return -1;
+        for(int i = dig.size() - 1; i >= idx + 1; i--) {
+            if(dig[i] > dig[idx]) {
+                swap(dig[i], dig[idx]);
+                break;
+            }
+        }
+        reverse(dig.begin() + idx + 1, dig.end());
         long long ans = stoll(dig);
-        return (ans > INT_MAX || ans <= n ? -1 : ans);
+        
+        return ans > INT_MAX ? -1 : ans;
     }
 };
