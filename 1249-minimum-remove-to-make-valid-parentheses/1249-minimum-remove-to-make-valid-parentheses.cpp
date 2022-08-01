bcfbe4c088561919644e391ead8084ace8f4fb5f
gauravksh <73842400+gauravksh@users.noreply.github.com>
2022-08-01 17:04:30 +0530

    Time: 46 ms (30.44%), Space: 12.2 MB (21.01%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     string minRemoveToMakeValid(string s) {
-        stack<pair<char,int>> st;
+        stack<int> st;
         string ans;
         vector<bool> flag(s.size());
         for(int i = 0; i < s.length(); i++) {
-            if(s[i] == '(') st.push({'(', i});
+            if(s[i] == '(') st.push(i);
             else if(s[i] == ')') {
-                if(!st.empty() && st.top().first == '(') {
-                    flag[st.top().second] = true;
+                if(!st.empty() && s[st.top()] == '(') {
+                    flag[st.top()] = true;
                     flag[i] = true;
                     st.pop();
                 }
                 else {
-                    st.push({')', i});
+                    st.push(i);
                     flag[i] = false;
                 }
             }
