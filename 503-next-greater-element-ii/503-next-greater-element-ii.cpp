cda520456f8a21fa70b0b837af78740cec23d36c
gauravksh <73842400+gauravksh@users.noreply.github.com>
2022-08-01 16:19:31 +0530

    Time: 30 ms (98.28%), Space: 24.1 MB (43.36%) - LeetHub

--- 503-next-greater-element-ii/503-next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/503-next-greater-element-ii.cpp
@@ -5,17 +5,16 @@ public:
         int n = nums.size();
         vector<int> ans(n);
         st.push(-1);
-        for(int i = 2*n-1; i >= 0; i--) {
-            while(st.size() > 1 && st.top() <= nums[i%n]) st.pop();
-            ans[i%n] = st.top();
-            st.push(nums[i%n]);
+        for(int i = n-1; i >= 0; i--) {
+            while(st.size() > 1 && st.top() <= nums[i]) st.pop();
+            ans[i] = st.top();
+            st.push(nums[i]);
         }
         for(int i = n-1; i >= 0; i--) {
             while(st.size() > 1 && st.top() <= nums[i]) st.pop();
             ans[i] = st.top();
             st.push(nums[i]);
         }
-        
         return ans;
     }
 };
