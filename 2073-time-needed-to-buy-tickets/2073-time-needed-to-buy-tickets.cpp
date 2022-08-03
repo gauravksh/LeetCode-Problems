cf85afd338f029205e23d96b965c395caa9e72a6
gauravksh <73842400+gauravksh@users.noreply.github.com>
2022-08-03 14:28:12 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (68.79%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
@@ -4,11 +4,14 @@ public:
         int ans = 0;
         int val = tickets[k];
         for(int i = 0; i < tickets.size(); i++) {
-            if(tickets[i] <= val - 1) ans += tickets[i];
-            else ans += val - 1;
-        }
-        for(int i = 0; i <= k; i++) {
-            if(tickets[i] >= val) ans++;
+            if(i <= k) {
+                if(tickets[i] <= val) ans += tickets[i];
+                else ans += val;
+            }
+            else {
+                if(tickets[i] < val) ans += tickets[i];
+                else ans += val - 1;
+            }
         }
         return ans;
     }
