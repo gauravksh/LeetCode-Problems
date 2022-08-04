51fbc98fa53d11139d02a203b085b8ac3b7c64fe
gauravksh <73842400+gauravksh@users.noreply.github.com>
2022-08-04 13:51:10 +0530

    Time: 49 ms (69.32%), Space: 20.6 MB (44.59%) - LeetHub

---
+++ 1670-design-front-middle-back-queue/1670-design-front-middle-back-queue.cpp
@@ -0,0 +1,119 @@
+class FrontMiddleBackQueue {
+public:
+    deque<int> list1;
+    deque<int> list2;
+    int size;
+    
+    FrontMiddleBackQueue() {
+        size = 0;        
+    }
+    
+    void pushFront(int val) {
+        
+        if(list1.size()- list2.size() == 0)
+        {
+            list1.push_front(val);
+        }
+        else
+        {
+            list1.push_front(val);
+            list2.push_front(list1.back());
+            list1.pop_back();
+        }
+        
+        size++;
+    }
+    
+    void pushMiddle(int val) {
+        
+        if(list1.size() - list2.size() == 0)
+        {
+            list1.push_back(val);
+        }
+        else
+        {
+            list2.push_front(list1.back());
+            list1.pop_back();
+            list1.push_back(val);
+        }
+        
+        size++;
+    }
+    
+    void pushBack(int val) {
+            
+        if(list1.size() - list2.size() == 0)
+        {
+            list2.push_back(val);
+            list1.push_back(list2.front());
+            list2.pop_front();
+        }
+        else
+        {
+            list2.push_back(val);
+        }
+        
+        size++;
+    }
+    
+    int popFront() {
+
+        if(size ==0) return -1;
+        
+        int val = list1.front();
+        list1.pop_front();
+        
+        if(list1.size()<list2.size())
+        {
+            list1.push_back(list2.front());
+            list2.pop_front();
+        }
+        
+        size--;
+        return val;
+    }
+    
+    int popMiddle() {
+        
+        if(size ==0) return -1;
+        
+        int val = list1.back();
+        list1.pop_back();
+
+        if(list1.size() < list2.size())
+        {
+            list1.push_back(list2.front());
+            list2.pop_front();
+        }
+        
+        size--;
+        return val;
+        
+    }
+    
+    int popBack() {
+        
+        if(size ==0) return -1;
+        
+        int val;
+        if(size==1)
+        {
+            val = list1.front();
+            list1.pop_front();
+        }
+        else
+        {
+            val = list2.back();
+            list2.pop_back();
+
+            if(list1.size() - list2.size() > 1)
+            {
+                list2.push_front(list1.back());
+                list1.pop_back();
+            }                   
+        }
+        
+        size--;
+        return val;       
+    }
+};
