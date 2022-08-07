f6f1e2a891b1de9d327c5361cedfe91be7caac84
gauravksh <73842400+gauravksh@users.noreply.github.com>
2022-08-07 21:57:02 +0530

    Time: 819 ms (59.63%), Space: 163.8 MB (81.84%) - LeetHub

--- 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.cpp
+++ 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.cpp
@@ -1,21 +1,12 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
-
 class Solution {
 public:
     TreeNode* getTargetCopy(TreeNode* original, TreeNode* cloned, TreeNode* target) {
-        TreeNode* ans = nullptr;
-        if(original == target) return cloned;
-        if(original->left) ans = getTargetCopy(original->left, cloned->left, target);
-        if(ans) return ans;
-        if(original->right) ans = getTargetCopy(original->right, cloned->right, target);
-        return ans;
+        if (!original) return nullptr;
+        if (original == target) return cloned;
+        TreeNode* node = getTargetCopy(original->left, cloned->left, target);
+        if (node) return node;
+        node = getTargetCopy(original->right, cloned->right, target);
+        if (node) return node;
+        return nullptr;
     }
 };
