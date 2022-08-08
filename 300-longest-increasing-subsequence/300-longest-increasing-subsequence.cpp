0769c73ad519d35c3b426a74b740e5242aef3299
gauravksh <73842400+gauravksh@users.noreply.github.com>
2022-08-08 14:58:04 +0530

    Time: 8 ms (95.75%), Space: 10.3 MB (88.72%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -4,12 +4,11 @@ public:
         vector<int > v;
         v.push_back(nums[0]);
         for(int i = 1;i < nums.size(); i++){
-            if(nums[i] > v.back()) v.push_back(nums[i]);
-            else {
-                auto idx = lower_bound(v.begin(), v.end(),nums[i]);
-                *idx = nums[i];
-            }
+            auto idx = lower_bound(v.begin(), v.end(),nums[i]);
+            if (idx == v.end()) v.push_back(nums[i]);
+            else *idx = nums[i];
         }
+        
         return v.size();
     }
 };
