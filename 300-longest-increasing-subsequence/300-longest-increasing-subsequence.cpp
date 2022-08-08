bd7dc753782b3c06ba2676625f2d4ee29a649b3c
gauravksh <73842400+gauravksh@users.noreply.github.com>
2022-08-08 14:56:04 +0530

    Time: 10 ms (93.16%), Space: 10.4 MB (69.86%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,35 +1,15 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-    //     set<pair<int,int>> s;
-    //     int num;
-    //     int ans = 1;
-    //     for(int i = 0; i < nums.size(); i++) {
-    //         num = nums[i];
-    //         auto lowBound = s.lower_bound({num, i});
-    //         // lowBound--;
-    //         cout << lowBound->first;
-    //         while(lowBound != s.begin() && (lowBound->first < num)) {
-    //             lowBound--;
-    //         }
-    //         if(lowBound == s.begin()) s.insert({num, 1});
-    //         else {
-    //             s.insert({num, lowBound->second + 1});
-    //             ans = max(ans, lowBound->second + 1);
-    //         }
-    //     }
-    //     return ans;
-        int ans = 1;
-        int n = nums.size();
-        vector<int> dp(n, 1);
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (nums[j] < nums[i]) {
-                    dp[i] = max(dp[i], dp[j] + 1);
-                    ans = max(ans, dp[i]);
-                }
+        vector<int > v;
+        v.push_back(nums[0]);
+        for(int i = 1;i < nums.size(); i++){
+            if(nums[i] > v.back()) v.push_back(nums[i]);
+            else {
+                auto idx = lower_bound(v.begin(), v.end(),nums[i]);
+                *idx = nums[i];
             }
         }
-        return ans;
+        return v.size();
     }
 };
