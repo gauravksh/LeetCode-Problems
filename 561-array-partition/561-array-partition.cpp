2741a552175eff5a13cb8f4be56934c553216dfb
gauravksh <73842400+gauravksh@users.noreply.github.com>
2022-08-09 16:02:03 +0530

    Time: 88 ms (40.15%), Space: 28.2 MB (48.04%) - LeetHub

--- 561-array-partition/561-array-partition.cpp
+++ 561-array-partition/561-array-partition.cpp
@@ -5,7 +5,7 @@ public:
         int ans = 0;
         int n = nums.size();
         for(int i = 0; i < n-1; i += 2) {
-            ans += min(nums[i], nums[i+1]);
+            ans += nums[i];
         }
         return ans;
     }
