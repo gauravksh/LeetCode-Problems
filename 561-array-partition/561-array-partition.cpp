9af140ab9d738b4eb9e37462c8865ff38b15b0c4
gauravksh <73842400+gauravksh@users.noreply.github.com>
2022-08-09 16:00:23 +0530

    Time: 100 ms (21.09%), Space: 28.2 MB (48.04%) - LeetHub

---
+++ 561-array-partition/561-array-partition.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int arrayPairSum(vector<int>& nums) {
+        sort(begin(nums), end(nums));
+        int ans = 0;
+        int n = nums.size();
+        for(int i = 0; i < n-1; i += 2) {
+            ans += min(nums[i], nums[i+1]);
+        }
+        return ans;
+    }
+};
