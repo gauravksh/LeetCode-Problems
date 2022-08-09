d320eb629636c8a5e0b72c7547d0c74c0ca2548c
gauravksh <73842400+gauravksh@users.noreply.github.com>
2022-08-09 15:37:50 +0530

    Time: 57 ms (59.33%), Space: 20 MB (32.78%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -1,39 +1,23 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        stack<int> st;
         sort(intervals.begin(), intervals.end());
         vector<vector<int>> ans;
-        int start, end;
+        int len = 0;
         for(auto x : intervals) {
-            if(st.size() && st.top() >= x[0]) {
-                if(x[1] > st.top()) {
-                    st.pop();
-                    st.push(x[1]);
-                }
+            if(len == 0) {
+                ans.push_back(x);
+                len++;
             }
-            else {
-                if(!st.empty()) {
-                    end = st.top();
-                    st.pop();
-                    start = st.top();
-                    st.pop();
-                    ans.push_back({start,end});
-                    st.push(x[0]);
-                    st.push(x[1]);
+            else{
+                if(ans[len-1][1] >= x[0]) {
+                    if(ans[len-1][1] < x[1]) ans[len-1][1] = x[1];
                 }
                 else {
-                    st.push(x[0]);
-                    st.push(x[1]);
+                    ans.push_back(x);
+                    len++;
                 }
             }
-        }
-        if(!st.empty()) {
-            end = st.top();
-            st.pop();
-            start = st.top();
-            st.pop();
-            ans.push_back({start,end});
         }
         return ans;
     }
