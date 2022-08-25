4ef389c337f1e9314ec200d1ae79f0fa4dba1d35
gauravksh <73842400+gauravksh@users.noreply.github.com>
2022-08-25 20:23:07 +0530

    Time: 30 ms (43.30%), Space: 8.9 MB (39.49%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char, int> mp;
+        for(char ch : ransomNote) {
+            mp[ch]++;
+        }
+        for(char ch : magazine) {
+            if(mp.find(ch) != mp.end()) {
+                mp[ch]--;
+                if(!mp[ch]) mp.erase(ch);
+                if(mp.empty()) return true;
+            }
+        }
+        
+        return false;
+    }
+};
