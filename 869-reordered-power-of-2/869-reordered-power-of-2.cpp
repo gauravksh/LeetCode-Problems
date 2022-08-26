ce3b46f9ce8228bbc62140bce617678f5e4a2d4c
gauravksh <73842400+gauravksh@users.noreply.github.com>
2022-08-26 20:14:44 +0530

    Time: 4 ms (61.58%), Space: 6.4 MB (36.72%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.cpp
+++ 869-reordered-power-of-2/869-reordered-power-of-2.cpp
@@ -1,21 +1,17 @@
-class Solution {
-    bool isPowerOf2(string num) {
-        int n = stoi(num);
-        if((n & (n-1)) == 0) return true;
-        return false;
-    }
-    
+class Solution {   
 public:
     bool reorderedPowerOf2(int n) {
-        string num = to_string(n);
-        sort(begin(num), end(num));
-        if(num[0] != '0' && isPowerOf2(num)) return true;
-        
-        while(next_permutation(num.begin(), num.end())) {
-            if(num[0] == '0') continue;
-            if(isPowerOf2(num)) return true;
+        unordered_set<string> mp;
+        int num;
+        string s;
+        for(int i = 0; i < 31; i++) {
+            num = 1 << i;
+            s = to_string(num);
+            sort(s.begin(), s.end());
+            mp.insert(s);
         }
-        
-        return false;
+        s = to_string(n);
+        sort(begin(s), end(s));
+        return mp.find(s) != mp.end();
     }
 };
