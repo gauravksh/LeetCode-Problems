69eb535d17b3583964b62666259e6f0c6aa45def
gauravksh <73842400+gauravksh@users.noreply.github.com>
2022-09-19 20:47:32 +0530

    Time: 187 ms (52.67%), Space: 33.9 MB (90.84%) - LeetHub

--- 609-find-duplicate-file-in-system/609-find-duplicate-file-in-system.cpp
+++ 609-find-duplicate-file-in-system/609-find-duplicate-file-in-system.cpp
@@ -1,24 +1,38 @@
 class Solution {
 public:
     vector<vector<string>> findDuplicate(vector<string>& paths) {
-        unordered_map<string, vector<string>> hash;
-        for (auto path : paths) {
-            stringstream ss(path);
-            string dir;
-            string file;
-            getline(ss, dir, ' ');
-            while (getline(ss, file, ' ')) {
-                string content = file.substr(file.find('(') + 1, file.find(')') - file.find('(') - 1);
-                string name = dir + '/' + file.substr(0, file.find('('));
-                hash[content].push_back(name);
+        
+        unordered_map<string, vector<string>> mp;
+        for (const string& path : paths) {
+            string pathStart;
+            int i = 0;
+            while (path[i] != ' ') {
+                pathStart.push_back(path[i]);
+                i++;
             }
+            i++;
+            
+            while (i < path.size()) {
+                string fileName, fileData;
+                while (path[i] != '(') {
+                    fileName.push_back(path[i]);
+                    i++;
+                }
+                i++;
+                while (path[i] != ')') {
+                    fileData.push_back(path[i]);
+                    i++;
+                }
+                i += 2;
+                mp[fileData].push_back(pathStart + "/" + fileName);
+            }     
         }
+        
         vector<vector<string>> res;
-        for (auto it = hash.begin(); it != hash.end(); it++) {
-            if (it->second.size() > 1) {
-                res.push_back(it->second);
-            }
-        }
+        for (const auto& [key, value] : mp)
+            if (value.size() > 1)
+                res.push_back(value);
+        
         return res;
     }
 };
